170ac6e25ec31d5ac65694462d0a82de37631738
Shiiovo <Shiiovo@users.noreply.github.com>
2025-05-07 00:19:41 +0000

    deploy: c51a9a2bdcf50853859e5830b9cbbd8032578a91

---
+++ assets/js/acecf23e.f4d0ceeb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/smartHospital/blog","blogTitle":"Blog","authorsListPath":"/smartHospital/blog/authors"}')}}]);
---
+++ assets/js/db63aa73.1e43b899.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2052],{9502:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/smartHospital/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
